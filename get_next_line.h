5754a2447ab8d2ff24882de61c98735116c6f4ca
Renan Batista <contato@renanbatista.com.br>
2023-06-11 17:48:59 -0300

    feat: add

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: r-afonso < r-afonso@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/11 17:33:46 by r-afonso          #+#    #+#             */
+/*   Updated: 2023/06/11 17:36:55 by r-afonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifdef BUFFER_SIZE
+#  define BS BUFFER_SIZE
+# else
+#  define BUFFER_SIZE 1
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_get_line(char *save);
+char	*ft_save(char *save);
+char	*ft_read_and_save(int fd, char *save);
+
+#endif
